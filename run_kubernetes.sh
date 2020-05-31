256bc7914f476e93e99c22b5aa816c95e4ddf0f6
Mudathir Lawal <nigercode@gmail.com>
2020-05-31 02:56:45 +0100

    ammend run_kubernetes.sh & add kubernetes console logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,13 +8,12 @@ dockerpath="nigercode/sklearn-microservice-api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes 
-kubectl create namespace microservices
-kubectl apply -f sklearn-pod.yml -n microservices
+kubectl run sklearn-pod --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods in the `microservices` namespace
-kubectl get pod -n microservices
+kubectl get pod 
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pods/sklearn-pod -n microservices 8000:80
+kubectl port-forward sklearn-pod 8000:80
